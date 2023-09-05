3c959d36fb398b36d5ef81840abb6bc57e536ccf
kisaragychihaya <hetongtech@hotmail.com>
2023-09-05 09:23:15 +0800

    change hgnetv2 backbone pretrained weight from ultralytics's repo  to YiKU‘s

--- utils/utils.py
+++ utils/utils.py
@@ -1,44 +1,50 @@
 import numpy as np
 from PIL import Image
 
-#---------------------------------------------------------#
+
+# ---------------------------------------------------------#
 #   将图像转换成RGB图像，防止灰度图在预测时报错。
 #   代码仅仅支持RGB图像的预测，所有其它类型的图像都会转化成RGB
-#---------------------------------------------------------#
+# ---------------------------------------------------------#
 def cvtColor(image):
     if len(np.shape(image)) == 3 and np.shape(image)[2] == 3:
-        return image 
+        return image
     else:
         image = image.convert('RGB')
-        return image 
+        return image
 
-#---------------------------------------------------#
+    # ---------------------------------------------------#
+
+
 #   对输入图像进行resize
-#---------------------------------------------------#
+# ---------------------------------------------------#
 def resize_image(image, size):
-    iw, ih  = image.size
-    w, h    = size
+    iw, ih = image.size
+    w, h = size
 
-    scale   = min(w/iw, h/ih)
-    nw      = int(iw*scale)
-    nh      = int(ih*scale)
+    scale = min(w / iw, h / ih)
+    nw = int(iw * scale)
+    nh = int(ih * scale)
 
-    image   = image.resize((nw,nh), Image.BICUBIC)
-    new_image = Image.new('RGB', size, (128,128,128))
-    new_image.paste(image, ((w-nw)//2, (h-nh)//2))
+    image = image.resize((nw, nh), Image.BICUBIC)
+    new_image = Image.new('RGB', size, (128, 128, 128))
+    new_image.paste(image, ((w - nw) // 2, (h - nh) // 2))
 
     return new_image, nw, nh
-    
-#---------------------------------------------------#
+
+
+# ---------------------------------------------------#
 #   获得学习率
-#---------------------------------------------------#
+# ---------------------------------------------------#
 def get_lr(optimizer):
     for param_group in optimizer.param_groups:
         return param_group['lr']
 
+
 def preprocess_input(image):
     image /= 255.0
     return image
+
 
 def show_config(**kwargs):
     print('Configurations:')
@@ -49,21 +55,22 @@ def show_config(**kwargs):
         print('|%25s | %40s|' % (str(key), str(value)))
     print('-' * 70)
 
+
 def download_weights(backbone, model_dir="./model_data"):
     import os
     from torch.hub import load_state_dict_from_url
-    
+
     download_urls = {
-        'mobilenet' : 'https://github.com/bubbliiiing/deeplabv3-plus-pytorch/releases/download/v1.0/mobilenet_v2.pth.tar',
-        'xception'  : 'https://github.com/bubbliiiing/deeplabv3-plus-pytorch/releases/download/v1.0/xception_pytorch_imagenet.pth',
-        'hgnetv2l': 'https://github.com/ultralytics/assets/releases/download/v0.0.0/rtdetr-l.pt',
-        "hgnetv2x":"https://github.com/ultralytics/assets/releases/download/v0.0.0/rtdetr-x.pt",
-        "yolov8s":"https://github.com/ultralytics/assets/releases/download/v0.0.0/yolov8s-cls.pt",
-        "yolov8m":"https://github.com/ultralytics/assets/releases/download/v0.0.0/yolov8m-cls.pt",
+        'mobilenet': 'https://github.com/bubbliiiing/deeplabv3-plus-pytorch/releases/download/v1.0/mobilenet_v2.pth.tar',
+        'xception': 'https://github.com/bubbliiiing/deeplabv3-plus-pytorch/releases/download/v1.0/xception_pytorch_imagenet.pth',
+        'hgnetv2l': 'https://github.com/VIRobotics/hgnetv2-deeplabv3/releases/download/v0.0.2-beta/hgnetv2l.pt',
+        "hgnetv2x": "https://github.com/VIRobotics/hgnetv2-deeplabv3/releases/download/v0.0.2-beta/hgnetv2x.pt",
+        "yolov8s": "https://github.com/ultralytics/assets/releases/download/v0.0.0/yolov8s-cls.pt",
+        "yolov8m": "https://github.com/ultralytics/assets/releases/download/v0.0.0/yolov8m-cls.pt",
 
     }
     url = download_urls[backbone]
-    
+
     if not os.path.exists(model_dir):
         os.makedirs(model_dir)
     load_state_dict_from_url(url, model_dir)

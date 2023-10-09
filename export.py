e710be467b739d04c69ebf2528deaa0bd5df91ce
kisaragychihaya <hetongtech@hotmail.com>
2023-10-09 14:51:06 +0800

    增加Paddle导出

--- export.py
+++ export.py
@@ -12,7 +12,11 @@ from nets.labs import Labs
 
 def export_onnx(net,f,imgsz=512,**kwargs):
     f=f+".onnx"
-    im = torch.zeros(1, *imgsz, 3).to('cpu')
+    batch=kwargs["batch"]
+    if batch>0:
+        im = torch.zeros(batch, imgsz,imgsz, 3).to('cpu')
+    else:
+        im = torch.zeros(1, imgsz,imgsz, 3).to('cpu')
     input_layer_names = ["images"]
     output_layer_names = ["output"]
 
@@ -29,6 +33,9 @@ def export_onnx(net,f,imgsz=512,**kwargs):
             pr = torch.argmax(pr, -1)
             return pr
     net=Net_with_post(net)
+    if batch<=0:
+        dynamic = {'images': {0: 'batch'}}
+        dynamic['output0'] = {0: 'batch'}
 
     print(f'Starting export with onnx {onnx.__version__}.')
     torch.onnx.export(net,
@@ -40,7 +47,7 @@ def export_onnx(net,f,imgsz=512,**kwargs):
                       do_constant_folding=True,
                       input_names=input_layer_names,
                       output_names=output_layer_names,
-                      dynamic_axes=None)
+                      dynamic_axes=dynamic or None)
 
     # Checks
     model_onnx = onnx.load(f)  # load onnx model
@@ -58,18 +65,35 @@ def export_onnx(net,f,imgsz=512,**kwargs):
         onnx.save(model_onnx, f)
     except ImportError:
         print(f'onnx-simplifier not installed. SKIP')
+    return f
 
 def export_openvino(net,f,imgsz=512,fp16=True,**kwargs):
     import openvino.runtime as ov  # noqa
     from openvino.tools import mo  # noqa
     fo=f+"_openvino"
     onnx_path=export_onnx(net,f,imgsz,**kwargs)
+    print("Start OpenVINO model export")
     ov_model = mo.convert_model(onnx_path,
                                 model_name="Labs",
                                 framework='onnx',
                                 compress_to_fp16=fp16)
     ov.serialize(ov_model, os.path.join(fo,"model.xml"))
+    print("OpenVINO model export at %s"%os.path.join(fo,"model.xml"))
     return os.path.join(fo,"model.xml")
+
+def export_paddle(net,f,imgsz=512,**kwargs):
+    import x2paddle  # noqa
+    from x2paddle.convert import pytorch2paddle  # noqa
+    fo = f + "_paddle"
+    batch = kwargs["batch"]
+    if batch > 0:
+        im = torch.zeros(batch, imgsz, imgsz, 3).to('cpu')
+    else:
+        im = torch.zeros(1, imgsz, imgsz, 3).to('cpu')
+    print("Start Paddle model export")
+    pytorch2paddle(module=net, save_dir=fo, jit_type='trace', input_examples=[im])
+    print("Paddle model export at %s" % fo)
+    return fo
 
 
 if __name__ == "__main__":
@@ -77,6 +101,7 @@ if __name__ == "__main__":
     parser.add_argument('-c', '--config',default="config.ini")
     parser.add_argument('-f', '--format',nargs="*" ,default=["onnx"])
     parser.add_argument('--half', action='store_true')
+    parser.add_argument("-b",'--batch', type=int,default=1)
     config = configparser.ConfigParser()
     args = parser.parse_args()
     if os.path.exists(args.config):
@@ -117,5 +142,5 @@ if __name__ == "__main__":
     mod = sys.modules[__name__]
     for format in FORMATS:
         func = getattr(mod, "export_"+format)
-        func(net,os.path.join(SAVE_PATH,"best"),IMGSZ,fp16=args.half)
+        func(net,os.path.join(SAVE_PATH,"best"),IMGSZ,fp16=args.half,batch=args.batch)
 

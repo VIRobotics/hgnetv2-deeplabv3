5e0c82dce4216e7c5bd17e4905a9072dab8c7dc4
kisaragychihaya <hetongtech@hotmail.com>
2023-11-07 10:50:10 +0800

    增加 bubbliiiing/unet-pytorch 模型的支持

--- yiku/nets/third_party/UNet/__init__.py
+++ yiku/nets/third_party/UNet/__init__.py
@@ -1,4 +1,12 @@
-from .hgnetv2_unet import UNet
-
+def UNet(**kwargs):
+    if "backbone" in kwargs.keys():
+        if kwargs["backbone"]=="resnet50":
+            from .bubbliiiing_unet import Unet
+            return Unet(kwargs["num_classes"],pretrained=kwargs["pretrained"],backbone="resnet50")
+        elif kwargs["backbone"]=="vgg":
+            from .bubbliiiing_unet import Unet
+            return Unet(kwargs["num_classes"], pretrained=kwargs["pretrained"], backbone="vgg")
+    from .hgnetv2_unet import UNet as Unet
+    return Unet(kwargs["num_classes"], pretrained=kwargs["pretrained"])
 
 
